7af12c6edbbc2a70e57a99295394f47e8ba273d8
Oleksandr <lysakova@yahoo.com>
2024-07-01 10:26:59 +0200

    feat: base setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+process.on('uncaughtException', (err) => {
+  console.error(err.name, err.message);
+  console.log('UNHANDLER EXEPTION! Shutting down...');
+
+  process.exit(1);
+});
+
+const app = express();
+
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  //   const error = new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
+  //   next(error);
+
+  console.log(`Can't find ${req.originalUrl} on this server!`);
+  next();
+});
+
+export { app };
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { app } from './app';
+
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err: Error) => {
+  console.error(err.name, err.message);
+  console.log('UNHANDLER REJECTION! Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
