9c0fd7595d5ab9d65fdde8abda2fa4947fb49dba
Oleksandr <lysakova@yahoo.com>
2024-07-04 05:55:04 +0200

    feat: add mock users tests

--- src/entities/users/tests/mock.test.ts
+++ src/entities/users/tests/mock.test.ts
@@ -1,4 +1,8 @@
 import supertest from 'supertest';
+import * as UserService from '../users.service';
+import { app } from '../../../app';
+import pool from '../../../config/db.config';
+import { HTTP_STATUS_CODES } from '../../../utils/statusCodes';
 
 const postData = {
   name: 'test',
@@ -7,8 +11,23 @@ const postData = {
   confirmPassword: 'test',
 };
 
+afterAll(async () => {
+  await pool.end();
+});
+
 describe('user', () => {
   describe('user creation', () => {
-    test('should create a user', () => {});
+    test('should create a user', async () => {
+      //@ts-expect-error description here
+      const createUserServiceMock = jest.spyOn(UserService, 'createUser').mockResolvedValueOnce(postData);
+
+      const response = await supertest(app).post('/api/v1/users').send(postData);
+
+      expect(response.statusCode).toBe(HTTP_STATUS_CODES.CREATED_201);
+      expect(response.body.data).toEqual(postData);
+      expect(createUserServiceMock).toHaveBeenCalledWith(postData);
+
+      createUserServiceMock.mockRestore();
+    });
   });
 });
