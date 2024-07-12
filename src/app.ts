3a352710a469ce63ff9c416191dddbf2177fe826
Oleksandr <lysakova@yahoo.com>
2024-07-12 10:32:14 +0200

    style: add some space in app.ts

--- src/app.ts
+++ src/app.ts
@@ -25,17 +25,22 @@ const app = express();
 
 // security headers
 app.use('/api', helmet());
+
 // rate limiter
 app.use('/api', limiter);
+
 // loger in development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 // body parser
 app.use(express.json({ limit: '10kb' }));
+
 // api routes
 app.use('/api/v1', usersRouter);
 app.use('/api/v1', authRouter);
+
 // route not found on server send error response
 app.use('*', (req: Request, _res: Response, next: NextFunction) => {
   const error = new AppError(`Can't find ${req.originalUrl} on this server!`, HTTP_STATUS_CODES.NOT_FOUND_404);
