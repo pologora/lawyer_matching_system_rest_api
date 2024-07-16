86c7799424615f393bd83b2553b7de653682cc3e
Oleksandr <lysakova@yahoo.com>
2024-07-16 11:04:02 +0200

    feat: update create database table script

--- src/config/databaseTables/createTables.ts
+++ src/config/databaseTables/createTables.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-import { exit } from 'process';
 import {
   usersTable,
   lawyersTable,
@@ -11,44 +10,29 @@ import {
   reviewsTable,
 } from './tablesQueries';
 import pool from '../db.config';
-import { specializations } from './specjalizations';
-import { createInsertSpecializationsQuery, insertSpecializations } from './insertSpecializations';
 
-const createDatabaseTablesIfNotExists = async (connection: typeof pool) => {
-  await connection.query(usersTable);
+export const createDatabaseTablesIfNotExists = async () => {
+  await pool.query(usersTable);
   console.log('Users table created successfully.');
 
-  await connection.query(lawyersTable);
+  await pool.query(lawyersTable);
   console.log('Lawyers table created successfully.');
 
-  await connection.query(specializationsTable);
+  await pool.query(specializationsTable);
   console.log('Specializations table created successfully.');
 
-  await connection.query(lawyerSpecializationsTable);
+  await pool.query(lawyerSpecializationsTable);
   console.log('Lawyer specializations table created successfully.');
 
-  await connection.query(casesTable);
+  await pool.query(casesTable);
   console.log('Cases table created successfully.');
 
-  await connection.query(clientProfilesTable);
+  await pool.query(clientProfilesTable);
   console.log('Client profiles table created successfully.');
 
-  await connection.query(messagesTable);
+  await pool.query(messagesTable);
   console.log('Messages table created successfully.');
 
-  await connection.query(reviewsTable);
+  await pool.query(reviewsTable);
   console.log('Reviews  table created successfully.');
-};
-
-export const runTablesSetup = async () => {
-  try {
-    await createDatabaseTablesIfNotExists(pool);
-    await insertSpecializations(createInsertSpecializationsQuery(specializations));
-  } catch (error) {
-    console.error('Error creating table:', error);
-  } finally {
-    if (process.env.NODE_ENV !== 'test') {
-      exit(1);
-    }
-  }
 };
--- src/config/databaseTables/setupDatabaseTablesAndValues.ts
+++ src/config/databaseTables/setupDatabaseTablesAndValues.ts
@@ -1,3 +1,20 @@
-import { runTablesSetup } from './createTables';
+/* eslint-disable no-console */
+import { exit } from 'process';
+import { createDatabaseTablesIfNotExists } from './createTables';
+import { createInsertSpecializationsQuery, insertSpecializations } from './insertSpecializations';
+import { specializations } from './specjalizations';
+
+const runTablesSetup = async () => {
+  try {
+    await createDatabaseTablesIfNotExists();
+    await insertSpecializations(createInsertSpecializationsQuery(specializations));
+  } catch (error) {
+    console.error('Error creating table:', error);
+  } finally {
+    if (process.env.NODE_ENV !== 'test') {
+      exit(1);
+    }
+  }
+};
 
 runTablesSetup();
