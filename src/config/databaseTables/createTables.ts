31a2ff4e4a711d275fd2eb079368ed94908aec34
Oleksandr <lysakova@yahoo.com>
2024-07-16 09:54:27 +0200

    feat: add all tables creatiion queries

--- src/config/databaseTables/createTables.ts
+++ src/config/databaseTables/createTables.ts
@@ -6,27 +6,44 @@ import {
   lawyerSpecializationsTable,
   specializationsTable,
   casesTable,
+  clientProfilesTable,
+  messagesTable,
+  reviewsTable,
 } from './tablesQueries';
 import pool from '../db.config';
 import { specializations } from './specjalizations';
 import { createInsertSpecializationsQuery, insertSpecializations } from './insertSpecializations';
 
 const createDatabaseTablesIfNotExists = async (connection: typeof pool) => {
+  await connection.query(usersTable);
+  console.log('Users table created successfully.');
+
+  await connection.query(lawyersTable);
+  console.log('Lawyers table created successfully.');
+
+  await connection.query(specializationsTable);
+  console.log('Specializations table created successfully.');
+
+  await connection.query(lawyerSpecializationsTable);
+  console.log('Lawyer specializations table created successfully.');
+
+  await connection.query(casesTable);
+  console.log('Cases table created successfully.');
+
+  await connection.query(clientProfilesTable);
+  console.log('Client profiles table created successfully.');
+
+  await connection.query(messagesTable);
+  console.log('Messages table created successfully.');
+
+  await connection.query(reviewsTable);
+  console.log('Reviews  table created successfully.');
+};
+
+export const runTablesSetup = async () => {
   try {
-    await connection.query(usersTable);
-    console.log('Users table created successfully.');
-
-    await connection.query(lawyersTable);
-    console.log('Lawyers table created successfully.');
-
-    await connection.query(specializationsTable);
-    console.log('Specializations table created successfully.');
-
-    await connection.query(lawyerSpecializationsTable);
-    console.log('Lawyer specializations table created successfully.');
-
-    await connection.query(casesTable);
-    console.log('Cases table created successfully.');
+    await createDatabaseTablesIfNotExists(pool);
+    await insertSpecializations(createInsertSpecializationsQuery(specializations));
   } catch (error) {
     console.error('Error creating table:', error);
   } finally {
@@ -34,9 +51,4 @@ const createDatabaseTablesIfNotExists = async (connection: typeof pool) => {
       exit(1);
     }
   }
-};
-
-export const runTablesSetup = () => {
-  createDatabaseTablesIfNotExists(pool);
-  insertSpecializations(createInsertSpecializationsQuery(specializations));
 };
--- src/config/databaseTables/tablesQueries.ts
+++ src/config/databaseTables/tablesQueries.ts
@@ -1,5 +1,5 @@
 const usersTable = `
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id int AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS user (
 `;
 
 const lawyersTable = `
-CREATE TABLE IF NOT EXISTS lawyer_profile(
+CREATE TABLE IF NOT EXISTS lawyer_profiles(
 id int AUTO_INCREMENT PRIMARY KEY,
 user_id int NOT NULL UNIQUE,
 license_number VARCHAR(255),
@@ -25,28 +25,40 @@ last_name VARCHAR(100),
 city VARCHAR(100),
 region VARCHAR(100),
 rating int,
-FOREIGN KEY (user_id)
-    REFERENCES user(id)
-    ON DELETE CASCADE
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+INDEX (user_id)
 );
 `;
 
 const specializationsTable = `
-CREATE TABLE IF NOT EXISTS specialization (
+CREATE TABLE IF NOT EXISTS specializations (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 `;
 
 const lawyerSpecializationsTable = `
-CREATE TABLE IF NOT EXISTS lawyer_specialization (
+CREATE TABLE IF NOT EXISTS lawyer_specializations (
     id INT AUTO_INCREMENT PRIMARY KEY,
     lawyer_id INT NOT NULL,
     specialization_id INT NOT NULL,
-    FOREIGN KEY (lawyer_id) REFERENCES lawyer_profile(id) ON DELETE CASCADE,
-    FOREIGN KEY (specialization_id) REFERENCES specialization(id),
-    UNIQUE KEY unique_specialization (lawyer_id, specialization_id)
+    FOREIGN KEY (lawyer_id) REFERENCES lawyer_profiles(id) ON DELETE CASCADE,
+    FOREIGN KEY (specialization_id) REFERENCES specializations(id) ON DELETE CASCADE,
+    UNIQUE KEY unique_specialization (lawyer_id, specialization_id),
+    INDEX (lawyer_id),
+    INDEX (specialization_id)
 );
+`;
+
+const clientProfilesTable = `
+CREATE TABLE IF NOT EXISTS client_profiles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id int,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    INDEX (user_id)
+    );
 `;
 
 const casesTable = `
@@ -55,9 +67,11 @@ const casesTable = `
     client_id int,
     lawyer_id int,
     description text,
-    status ENUM ("open", "closed", "pending") DEFAULT "open",
+    status ENUM("open", "closed", "pending") DEFAULT "open",
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX (client_id),
+    INDEX (lawyer_id)
     );
 `;
 
@@ -65,12 +79,40 @@ const reviewsTable = `
     CREATE TABLE IF NOT EXISTS reviews (
     id INT AUTO_INCREMENT PRIMARY KEY,
     client_id int,
-    lawyer_id int,
+    lawyer_id int NOT NULL,
     description text,
     status ENUM ("open", "closed", "pending") DEFAULT "open",
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES client_profiles(id) ON DELETE SET NULL,
+    FOREIGN KEY (lawyer_id) REFERENCES lawyer_profiles(id) ON DELETE CASCADE,
+    INDEX (client_id),
+    INDEX (lawyer_id)
     );
 `;
 
-export { usersTable, lawyersTable, lawyerSpecializationsTable, specializationsTable, casesTable };
+const messagesTable = `
+    CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id int,
+    lawyer_id int,
+    message text,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES client_profiles(id) ON DELETE SET NULL,
+    FOREIGN KEY (lawyer_id) REFERENCES lawyer_profiles(id) ON DELETE SET NULL,
+    INDEX (client_id),
+    INDEX (lawyer_id)
+    );
+`;
+
+export {
+  usersTable,
+  lawyersTable,
+  lawyerSpecializationsTable,
+  specializationsTable,
+  casesTable,
+  clientProfilesTable,
+  messagesTable,
+  reviewsTable,
+};
