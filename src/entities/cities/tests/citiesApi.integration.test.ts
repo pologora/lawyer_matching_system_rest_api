5f7c3090085216a2f83b1671bd0874f8b2280214
Oleksandr <lysakova@yahoo.com>
2024-07-26 11:18:21 +0200

    test: add /cities and /regions integration tests

---
+++ src/entities/cities/tests/citiesApi.integration.test.ts
@@ -0,0 +1,22 @@
+import supertest from 'supertest';
+import pool from '../../../config/db.config';
+import { app } from '../../../app';
+import { HTTP_STATUS_CODES } from '../../../utils/statusCodes';
+
+afterAll(async () => {
+  await pool.end();
+});
+
+describe('Test GET /cities?regionId=<id>', () => {
+  test('Should respond with 200 success', async () => {
+    const regionId = 2;
+
+    const response = await supertest(app)
+      .get(`/api/v1/cities?regionId=${regionId}`)
+      .expect(HTTP_STATUS_CODES.SUCCESS_200)
+      .expect('Content-Type', /json/);
+
+    expect(response.body).toHaveProperty('status', 'success');
+    expect(response.body).toHaveProperty('message', 'Cities retrieved successfully');
+  });
+});
