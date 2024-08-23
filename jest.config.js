19fbeeee4c0a18eead9985ea18f8d59533548e1a
Oleksandr <lysakova@yahoo.com>
2024-08-23 08:21:01 +0200

    feat: add coverage folder to .gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'js'],
 };
