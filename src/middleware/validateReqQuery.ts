7a5a00097833b7a80bbbd3c74518845a494e0a9e
Oleksandr <lysakova@yahoo.com>
2024-08-29 12:16:44 +0200

    fix: remove mutating req.body object

--- src/middleware/validateReqQuery.ts
+++ src/middleware/validateReqQuery.ts
@@ -5,13 +5,12 @@ import { HTTP_STATUS_CODES } from '../config/statusCodes';
 
 export const validateReqQuery = (schema: Joi.ObjectSchema) => {
   return (req: Request, _res: Response, next: NextFunction) => {
-    const { error, value } = schema.validate(req.query);
+    const { error } = schema.validate(req.query);
 
     if (error) {
       return next(new AppError(error.message, HTTP_STATUS_CODES.BAD_REQUEST_400));
     }
 
-    req.body = value;
     next();
   };
 };
