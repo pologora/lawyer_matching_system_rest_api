1735099dd2413b0a9c413814138029c0c139df43
Oleksandr <lysakova@yahoo.com>
2024-08-29 08:40:54 +0200

    style: remove curly brackets form asyncErrorCatch function

--- src/utils/errors/asyncErrorCatch.ts
+++ src/utils/errors/asyncErrorCatch.ts
@@ -2,6 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 type AsyncRequestHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 
-export const asyncErrorCatch = (fn: AsyncRequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => fn(req, res, next).catch(next);
-};
+export const asyncErrorCatch = (fn: AsyncRequestHandler) => (req: Request, res: Response, next: NextFunction) =>
+  fn(req, res, next).catch(next);
