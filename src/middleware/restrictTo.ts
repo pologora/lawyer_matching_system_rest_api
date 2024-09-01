76a065c6c02c9b69afad6749df2298a302c921f2
Oleksandr <lysakova@yahoo.com>
2024-09-01 12:57:05 +0200

    refactor: remove if statement

--- src/middleware/restrictTo.ts
+++ src/middleware/restrictTo.ts
@@ -8,7 +8,7 @@ import { IUser, UserRole } from '../types/IUser';
 export const restrictTo = (...allowedRoles: UserRole[]) =>
   asyncErrorCatch(async (req: Request, _res: Response, next: NextFunction) => {
     const user = req.user as IUser;
-    const role = (user.role as UserRole) || 'user';
+    const role = user.role as UserRole;
 
     if (!allowedRoles.includes(role)) {
       const error = new AppError('User does not have permissions', HTTP_STATUS_CODES.FORBIDDEN_403);
