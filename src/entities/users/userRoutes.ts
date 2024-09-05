d616b14fa0442fe88490b138099b257954f17962
Oleksandr <lysakova@yahoo.com>
2024-09-05 07:24:51 +0200

    feat: remove protection from users route for testing purposes

--- src/entities/users/userRoutes.ts
+++ src/entities/users/userRoutes.ts
@@ -2,12 +2,10 @@ import { Router } from 'express';
 
 import { asyncErrorCatch } from '../../utils/errors/asyncErrorCatch';
 import { uploadPhoto } from '../../middleware/uploadPhoto';
-import { protect } from '../../middleware/protect';
 import { resizeUserPhoto } from '../../middleware/resizeUserPhoto';
 import { getManyUsersSchema, userCreateSchema, userUpdateSchema } from './userValidation';
 import { validateReqBody } from '../../middleware/validateReqBody';
 import { validateReqQuery } from '../../middleware/validateReqQuery';
-import { restrictTo } from '../../middleware/restrictTo';
 import { validateIdParameter } from '../../middleware/validateIdParameter';
 import { hashPassword } from '../../utils/passwordManagement/hashPassword';
 import { UserController } from './UserController';
@@ -37,13 +35,13 @@ const uploadPhotoHandler = createUserHandler({ method: 'uploadPhoto' });
 
 usersRouter
   .route('/users')
-  .get(protect, restrictTo('admin'), validateReqQuery(getManyUsersSchema), asyncErrorCatch(getManyHandler))
-  .post(protect, restrictTo('admin'), validateReqBody(userCreateSchema), asyncErrorCatch(createOneHandler));
+  .get(validateReqQuery(getManyUsersSchema), asyncErrorCatch(getManyHandler))
+  .post(validateReqBody(userCreateSchema), asyncErrorCatch(createOneHandler));
 usersRouter
   .route('/users/:id')
-  .get(protect, restrictTo('admin'), asyncErrorCatch(getOneHandler))
-  .delete(protect, restrictTo('admin'), asyncErrorCatch(removeHandler))
-  .patch(protect, restrictTo('admin'), validateReqBody(userUpdateSchema), asyncErrorCatch(updateHandler));
+  .get(asyncErrorCatch(getOneHandler))
+  .delete(asyncErrorCatch(removeHandler))
+  .patch(validateReqBody(userUpdateSchema), asyncErrorCatch(updateHandler));
 usersRouter
   .route('/users/:id/upload-photo')
-  .patch(protect, uploadPhoto(), asyncErrorCatch(resizeUserPhoto), asyncErrorCatch(uploadPhotoHandler));
+  .patch(uploadPhoto(), asyncErrorCatch(resizeUserPhoto), asyncErrorCatch(uploadPhotoHandler));
